b999072d43108189e26de1414e485bcdc6eeb0d9
utviklerToni <h1de.toni@protonmail.com>
2021-05-27 18:23:46 +0200

    Standard grenavnet endret til main

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Text(
+        'Hello Node.js'
+      ),
     );
   }
 }
